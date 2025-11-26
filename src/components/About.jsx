9cb2a5187695207c2ee66ff6f9a2c43d0b7af68c
sharanraj2517-sudo <sharanraj2517@gmail.com>
2025-11-26 15:40:38 +0530

    updated about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,19 @@
-const About = () => {
-  return <h1>About Page</h1>;
-};
-export default About;
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function about(){
+
+
+  return(
+    <>
+    <div>
+      <h3>LOWEST PRICE GUARANTEE</h3>
+      <p>price Guarantee
+Why we do choose relax rent bikes, they are providing very quality and well maintained bikes. We are provide recent models stating from 2017 to 2019 bikes. We have lot of different type of bikes you can choose your dream bikes.</p>
+<h3>ROADSIDE ASSITANCE</h3>
+<p>git</p>
+    </div>
+    </>
+  )
+}
